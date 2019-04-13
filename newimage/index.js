b135dac0652318ac23d2c36c15b4a516c7f98a47
ptuton <ptuton@gmail.com>
2019-04-13 11:25:48 -0700

    testing addition of new function

---
+++ newimage/index.js
@@ -0,0 +1,13 @@
+module.exports = async function (context, eventGridEvent) {
+  context.log(typeof eventGridEvent);
+  context.log(eventGridEvent);
+
+  // Grab the file details from the event details 
+  // TODO
+  let message = eventGridEvent;
+
+  // Push the details to the newimage queue
+  context.log(message);   
+  context.bindings.outputNewImageQueue = message;
+  context.done();
+};
