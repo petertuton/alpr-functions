256e7ac27f4f7665aec0954e8d0a24036a1acdcd
ptuton <ptuton@gmail.com>
2019-04-13 10:59:32 -0700

    testing updates

--- processImage/index.js
+++ processImage/index.js
@@ -7,17 +7,17 @@ module.exports = async function (context, eventGridEvent) {
 
   // Check for confidence based on a provided threshold via env vars
   let confidence = process.env.CONFIDENCE_THRESHOLD || 90;  // TODO: Set to the result from the API call
-  let isDetected = (confidence_threshold >= CONFIDENCE_THRESHOLD)   ;
+  let isDetected = (confidence_threshold >= confidence)   ;
 
   // Send confident processing results to the appropriate queue
   if (isDetected) {
       let message = eventGridEvent;
       context.log(message);   
-      context.bindings.outputDetectedImageQueue = message;
+      context.bindings.outputSaveImageDetailsQueue = message;
   } else {
       let message = eventGridEvent;
       context.log(message);   
-      context.bindings.outputManualImageQueue = message;
+      context.bindings.outputManuallyProcessImageQueue = message;
   }
   context.done();
 
