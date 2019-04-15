3eaf3f9fa07e96857db901eb05af7a31f41c7f93
ptuton <ptuton@gmail.com>
2019-04-14 20:58:29 -0700

    update processImage with context logs

--- processImage/index.js
+++ processImage/index.js
@@ -3,17 +3,23 @@ module.exports = async function (context, eventGridEvent) {
 
   // Call the OpenALPR API
   // TODO
-  context.log("File:",eventGridEvent.data.url);
   let file = eventGridEvent.data.url;
-
-  // Check for confidence based on a provided threshold via env vars
   let confidence = 90;  // TODO: Set to the result from the API call
-  let isDetected = (confidence >= process.env.CONFIDENCE_THRESHOLD);
+  context.log("File:",file);
+
+  // Check for confidence based on a provided threshold
+  const confidence_threshold = process.env.CONFIDENCE_THRESHOLD;
+  let isDetected = (confidence >= confidence_threshold);
+  context.log("confidence_threshold:",confidence_threshold);
+  context.log("confidence:",confidence);
+  context.log("isDetected:",isDetected);
 
   // Send confident processing results to the appropriate queue
   if (isDetected) {
+      context.log("Pushing file to exportimage queue");
       context.bindings.outputExportImageDetailsQueue = file;
   } else {
+    context.log("Pushing file to manuallyprocess queue");
       context.bindings.outputManuallyProcessImageQueue = file;
   }
   context.done();
