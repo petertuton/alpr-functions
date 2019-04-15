82310fe31b7d4638a8be27ea39dcfdd787dad60e
ptuton <ptuton@gmail.com>
2019-04-15 13:13:09 -0700

    call openaplr api

--- processImage/index.js
+++ processImage/index.js
@@ -1,11 +1,27 @@
+const axios = require('axios');
+
 module.exports = async function (context, eventGridEvent) {
   context.log(eventGridEvent);
 
-  // Call the OpenALPR API
-  // TODO
-  let file = eventGridEvent.data.url;
+  // Confirm this event is for a 'PutBlockList' event
+  if (eventGridEvent.data.api != "PutBlockList") return;
+
+  // Call the OpenALPR API - just use the image_url function for now (with unauth access to the images container)
+  const openalpr_baseurl = process.env.OPENALPR_URL || "https://api.openalpr.com/v2/";
+  const openalpr_operation = "recognize_url";
+  const openalpr_country = "country=us";
+  const openalpr_key = "secret_key=" + process.env.OPENALPR_KEY;
+  const openalpr_imageurl = "image_url=" + eventGridEvent.data.url;
+  const openalpr_url = openalpr_baseurl + openalpr_operation + "?" + openalpr_country + "&" + openalpr_key + "&" + openalpr_imageurl;
+  
+  context.log("OpenALPR url:",openalpr_url);
+
+  axios.post(openalpr_url)
+    .then( (response) => {
+      context.log(response);
+    })
+  
   let confidence = 90;  // TODO: Set to the result from the API call
-  context.log("File:",file);
 
   // Check for confidence based on a provided threshold
   const confidence_threshold = process.env.CONFIDENCE_THRESHOLD;
@@ -16,7 +32,7 @@ module.exports = async function (context, eventGridEvent) {
 
   // Create the message
   const message = {
-    file
+    file: eventGridEvent.data.url
   };
 
   // Send processing results to the appropriate queue
